35c9d725ca09f22a2c02ebd847e225b61237c716
Malcolm Belfrom <mibelfrom@gmail.com>
2021-09-24 19:35:55 -0700

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Note-Taker
+<https://img.shields.io/badge/Node.js-v.14.17.6-brightgreen><https://img.shields.io/badge/Express.js-4.16.4-blue>
+--  
+## Table of Contents
+
+* Description 
+* Technology 
+* Images
+* Contact/Questions
+
+--
+## Description
+This application note-taker will allow you to modify, save, and delete notes in your local browser.  May it be a to-do list, journal entries, or random scribbling of thoughts, note taker will help you organize your ideas.
+
+-- 
+## Technology 
+* HTML
+* CSS
+* Javascript
+* Express.js
+* Node.js
+
+--
+## Images
+
+![Homework screenshots](./assets/images/Note-taker-1.PNG)
+![Homework screenshots](./assets/images/note-taker-2.PNG)
+
+--
+## Contact/Questions
+This password generator can be accessed from the following link (https://mbelfrom.github.io/Note-Taker/)
+
+Please contact me at (mibelfrom@gmail.com) with any questions, comments, or concerns.
+
+
--- server.js
+++ server.js
@@ -1,19 +1,64 @@
 const express = require('express');
 const app = express();
+const { clog } = require('./clog');
 const PORT = process.env.PORT || 3001;
-const path = require("path");
-const fs = require('fs');
-const { Server } = require('http');
+const path = require('path');
+const fs = require('fs/promises');
 
+app.use(clog);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.listen(PORT);
-module.exports = Server;
+
+app.get('/api/notes', async (req, res) => {
+  await fs.readFile('./db/db.json', 'utf8')
+  .then((data) => {
+    let parseData = JSON.parse(data)
+    res.json(parseData)
+  })
+});
+
+
+
+app.post('/api/notes', async (req, res) => { 
+  const newNotes = {
+    ...req.body,
+    id: 
+    Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+  };
+
+  const dataToRead = await fs.readFile('./db/db.json', 'utf8')
+  let parseData = JSON.parse(dataToRead)
+  parseData.push(newNotes)
+  const stringifyData = JSON.stringify(parseData, null, 4);
+  await fs.writeFile("./db/db.json", stringifyData);
+  res.send("Successfully added note!")
+});
+
+
+
+
+app.delete('/api/notes/:id', async (req, res) => { 
+  const dataToRead = await fs.readFile('./db/db.json', 'utf8')
+  let parseData = JSON.parse(dataToRead)
+  const filterNote = parseData.filter((note) => note.id !== req.params.id)
+  const stringifyData = JSON.stringify(filterNote, null, 4);
+  await fs.writeFile("./db/db.json", stringifyData);
+  res.send("Successfully deleted your note!")
+});
+
+
+app.listen(PORT, () => 
+  console.log(`http://localhost:${PORT}`)
+  );
